f164a994f3342963ab8dd9df1482cea1e65ebe56
rjvelazco <rjvelazco21@gmail.com>
2020-12-31 20:42:19 -0400

    Design-Layout-Main

---
+++ src/app/pages/pages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pages',
+  templateUrl: './pages.component.html',
+  styleUrls: ['../../assets/css/pages/login-register-lock.css']
+})
+export class PagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
